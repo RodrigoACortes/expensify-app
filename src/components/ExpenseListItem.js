77b8e54b5438c65816ae02e33e9ff216813dd4b1
YOUR NAME <rodrigocortes98@live.com>
2018-11-04 18:08:32 -0500

    added the hour that the expense was added at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -7,7 +7,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <Link className="list-item" to={`/edit/${id}`}>
     <div>
       <h3 className="list-item-title">{description}</h3>
-      <span className="list-tem__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
+      <span className="list-tem__sub-title">{moment(createdAt).format('MMMM Do, YYYY - h:mm a')}</span>
     </div>
     <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
   </Link>
